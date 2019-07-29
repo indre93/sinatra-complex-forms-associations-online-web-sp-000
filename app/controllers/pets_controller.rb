34cf510d96927e14042519bef1e6fb915cfc860d
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-29 05:57:50 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,9 +12,7 @@ class PetsController < ApplicationController
 
   post '/pets' do
     @pet = Pet.create(params["pet"])
-    if !params["owner"]["name"].empty?
-      @pet.owner = Owner.create(name: params["owner"]["name"])
-    end
+    @pet.owner = Owner.create(name: params["owner"]["name"])
     redirect to "pets/#{@pet.id}"
   end
 
@@ -29,7 +27,6 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
-    binding.pry
 
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
