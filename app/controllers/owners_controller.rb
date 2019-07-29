12a80b43ba855ad26b9906f0706c750154558657
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-29 05:21:22 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -33,7 +33,7 @@ class OwnersController < ApplicationController
     @owner = Owner.find(params[:id])
     @owner.update(params["owner"])
     if !params["pet"]["name"].empty?
-      @owner.pets << Pet.create(name: params["pet"]["name"])
+      @owner.pets << Pet.update(name: params["pet"])
     end
     redirect to "owners/#{@owner.id}"
   end
