469f716c18c6159a4d0ec5fc6164f89a4c27c8a3
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-29 04:44:22 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -29,10 +29,10 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do
+    @owner = Owner.find(params[:id])
     if !params[:owner].keys.include?("pet_ids")
       params[:owner]["pet_ids"] = []
     end
-    @owner = Owner.find(params[:id])
     @owner.update(params[:owner])
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
