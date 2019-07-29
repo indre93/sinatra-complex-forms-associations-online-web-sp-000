61e0e0d78da9196c307f9b8fac2fad4371b29fec
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-29 05:46:06 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -29,7 +29,6 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do
-    binding.pry
     @owner = Owner.find(params[:id])
     @owner.update(params["owner"])
     if !params["pet"]["name"].empty?
