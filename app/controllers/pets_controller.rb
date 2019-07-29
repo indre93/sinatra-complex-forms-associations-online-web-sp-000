ec718fcd624408b2ec22de78a828961bf17e8ccb
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-29 04:25:23 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -30,7 +30,7 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
-    @owner = Pet.find(params[:id])
+    @pet = Pet.find(params[:id])
 
     redirect to "pets/#{@pet.id}"
   end
