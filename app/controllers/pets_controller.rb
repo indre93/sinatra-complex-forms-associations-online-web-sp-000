4665c3a9090cc65593c6f4a976a509452217b45b
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-29 05:53:59 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -32,9 +32,7 @@ class PetsController < ApplicationController
     binding.pry
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
-    if !params["owner"]["name"].empty?
-      @pet.owner = Owner.create(name: params["owner"]["name"])
-    end
-    redirect to "pets/#{@pet.id}"
+    @pet.owner == Owner.create(name: params["owner"]["name"])
+   redirect to "pets/#{@pet.id}"
   end
 end
